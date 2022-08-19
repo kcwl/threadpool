2558ee73f90174fd602426e31ebcd13628b36b68
kcwl <767899122@qq.com>
2022-08-19 09:17:07 +0800

    优先级队列获取future类型问题

--- include/threadpool/task_traits.hpp
+++ include/threadpool/task_traits.hpp
@@ -17,7 +17,7 @@ namespace thread_pool
 
 			auto future = task_ptr->get_future();
 
-			default_task task([tk = std::move(task_ptr)]{ (*tk)();});
+			default_task task([tk = std::move(task_ptr)]{ (*tk)(); });
 
 			schedule.push(std::move(task));
 
@@ -29,7 +29,7 @@ namespace thread_pool
 	struct use_task<default_priority_task>
 	{
 		template<typename _Schedule, typename _Func, typename... _Args>
-		auto operator()(_Schedule& schedule, _Func&& func, _Args&&... args, std::size_t&& priority) -> std::future<std::invoke_result_t<_Func, _Args...>>&&
+		auto operator()(_Schedule& schedule, _Func&& func, _Args&&... args, std::size_t&& priority) -> std::future<std::invoke_result_t<_Func, _Args...>>
 		{
 			using return_type = std::invoke_result_t<_Func, _Args...>;
 
@@ -44,6 +44,18 @@ namespace thread_pool
 			return std::move(future);
 		}
 	};
+
+	template<typename _Tuple, std::size_t... I>
+	constexpr auto expand(_Tuple&& t, std::index_sequence<I...>)
+	{
+		return (std::get<I>(t), ...);
+	}
+
+	template<typename _Tuple, typename _Indices = std::make_index_sequence<std::tuple_size_v<_Tuple>>>
+	constexpr auto expand(_Tuple&& t)
+	{
+		return expand(std::forward<_Tuple>(t), _Indices{});
+	}
 
 	template<typename _Task, typename _Func, typename... _Args>
 	struct task_result
@@ -60,7 +72,7 @@ namespace thread_pool
 			return {};
 		}
 
-		using type = decltype(task_type(std::declval<_Func>(), std::declval<_Args...>()));
+		using type = decltype(task_type(std::declval<_Func>(), expand(std::tuple<_Args...>())));
 	};
 
 	template<typename _Task, typename _Func, typename... _Args>
