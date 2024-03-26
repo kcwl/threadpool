3bf7e7ae753bf01540de7111aa9a50e177098189
kcwl <767899122@qq.com>
2024-03-26 12:30:29 +0800

    complete wait for and until

--- include/threadpool/core/core.hpp
+++ include/threadpool/core/core.hpp
@@ -7,7 +7,6 @@
 #include <future>
 #include <memory>
 #include <mutex>
-#include <utility>
 #include <vector>
 
 namespace thread_pool
@@ -23,6 +22,7 @@ namespace thread_pool
 			using pool_t = pool_core<_Task, _Schedule, _Shutdown>;
 			using schedule_t = _Schedule<task_t>;
 			using thread_t = work_thread<pool_t>;
+			using shutdown_t = _Shutdown<pool_t>;
 
 			pool_core(std::size_t thread_size = std::thread::hardware_concurrency() * 2)
 				: thread_size_(thread_size)
@@ -31,12 +31,12 @@ namespace thread_pool
 
 		public:
 			template <typename _Func, typename... _Args>
-			auto schedule(_Func&& func, _Args&&... args) -> std::future<task_result_t<_Func, _Args...>>
+			auto schedule(_Func&& func, _Args&&... args) -> std::future<std::invoke_result_t<_Func, _Args...>>
 			{
 				std::unique_lock lk(mutex_);
 
 				if (is_shutdown_.load())
-					return std::future<task_result_t<_Func, _Args...>>{};
+					return std::future<std::invoke_result_t<_Func, _Args...>>{};
 
 				auto future = use_task<task_t>()(schedule_, std::forward<_Func>(func), std::forward<_Args>(args)...);
 
@@ -86,6 +86,8 @@ namespace thread_pool
 
 			void clear()
 			{
+				std::unique_lock lk(mutex_);
+
 				return schedule_.clear();
 			}
 
@@ -100,22 +102,34 @@ namespace thread_pool
 
 			template <typename _Time>
 			void wait_for(const _Time& tm)
-			{}
-
-			template <typename _Time>
-			void wait_until(const _Time& tm)
-			{}
-
-			void work_complete()
 			{
 				std::unique_lock lk(mutex_);
 
+				auto self = this->shared_from_this();
+
+				ternimate_cv_.wait_for(lk, tm,
+									   [self, this]() { return schedule_.empty() && active_thread_count_ == 0; });
+			}
+
+			template <typename _Time>
+			void wait_until(const _Time& tm)
+			{
+				std::unique_lock lk(mutex_);
+
+				auto self = this->shared_from_this();
+
+				ternimate_cv_.wait_until(lk, tm,
+										 [self, this]() { return schedule_.empty() && active_thread_count_ == 0; });
+			}
+
+			void work_complete()
+			{
 				active_thread_count_--;
 			}
 
 			void close()
 			{
-				return _Shutdown<pool_t>::shutdown(this->shared_from_this());
+				return shutdown_t::shutdown(this->shared_from_this());
 			}
 
 			void shutdown()
@@ -145,7 +159,7 @@ namespace thread_pool
 
 			std::condition_variable_any ternimate_cv_;
 
-			std::size_t active_thread_count_;
+			std::atomic_size_t active_thread_count_;
 		};
 	} // namespace impl
 } // namespace thread_pool
